c5a245be3c179ab1ca704b15eedcb1f07cae6302
Navneet Kumar <43682270+navneet2412@users.noreply.github.com>
2021-07-10 14:50:40 +0530

    Update database.js

--- util/database.js
+++ util/database.js
@@ -1,10 +1,5 @@
 const Sequelize = require("sequelize");
 
-// const sequelize = new Sequelize("lifecycle", "root", "", {
-//   dialect: "mysql",
-//   host: "localhost",
-//   logging: false,
-// });
 
 const sequelize = new Sequelize(
   "mysql://bbd8ad8f3d2a21:65f3d138@eu-cdbr-west-03.cleardb.net/heroku_0022d4615b5a70b?reconnect=true",
@@ -14,8 +9,6 @@ const sequelize = new Sequelize(
   }
 );
 
-// const sequelize = new Sequelize('mysql://lifecycle:lifecycle@wadhostinger.codebuckets.in/lifecycle?reconnect=true', {
-//   dialect: 'mysql',
-// });
+
 
 module.exports = sequelize;
